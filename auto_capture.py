ff528d4bb65402b4f21ac53918a0e2df8e26782b
jiunjiun69 <a902505@gmail.com>
2020-09-22 19:15:54 +0800

    '3'

--- auto_capture.py
+++ auto_capture.py
@@ -6,9 +6,9 @@ import time
 img_path = "./" #自己定義要儲存的路徑
 sleep_time = 60       #自己定義要拍攝的間隔
 
-cap = cv2.VideoCapture(0)        #開啟攝像頭
-
+cap = cv2.VideoCapture(1)        #開啟攝像頭
 n = 0
+
 while(1):
     # get a frame
     ret, frame = cap.read()
@@ -18,10 +18,11 @@ while(1):
     name = img_path + str(n) + ".png"
     cv2.imwrite(name, frame)   #儲存路徑
     n += 1
+    print(name + ":儲存成功")
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):   #如果按下q 就截圖儲存並退出
+    if cv2.waitKey(1) & 0xFF == ord('q'):   #如果按下q就退出
         cap.release()
         cv2.destroyAllWindows()
-	break
+        break
 
     time.sleep(sleep_time)
